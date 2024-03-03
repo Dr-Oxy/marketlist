a1d0c15ee5221926a844b92d66aae2460e2fb69b
droxy <chiomaotu22@gmail.com>
2024-03-03 10:09:48 +0100

    add stack navigation etc

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Splash from './screens/Splash';
 import AllList from './screens/AllList';
 import CreateList from './screens/CreateList';
 import ViewList from './screens/ViewList';
 
+import {enableScreens} from 'react-native-screens';
+enableScreens();
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Splash"
+        component={Splash}
+      />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="AllLists"
+        component={AllList}
+      />
+      <Stack.Screen name="CreateList" component={CreateList} />
+      <Stack.Screen name="ViewList" component={ViewList} />
+    </Stack.Navigator>
+  );
+}
+
 function App() {
   return (
-    <View>
-      <Text>Hello app</Text>
-    </View>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Splash" component={Splash} />
-    //     <Stack.Screen name="AllList" component={AllList} />
-    //     <Stack.Screen name="CreateList" component={CreateList} />
-    //     <Stack.Screen name="ViewList" component={ViewList} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
 
