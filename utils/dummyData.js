55979a5ecf92d5046b8332470c593d96d6353317
droxy <chiomaotu22@gmail.com>
2024-03-03 10:29:28 +0100

    added dummydata

---
+++ utils/dummyData.js
@@ -0,0 +1,92 @@
+export const measures = [
+  {
+    id: 1,
+    label: 'bag(s)',
+    value: 'bag(s)',
+  },
+  {
+    id: 2,
+    label: 'basket(s)',
+    value: 'basket(s)',
+  },
+  {
+    id: 3,
+    label: 'bucket(s)',
+    value: 'bucket(s)',
+  },
+  {
+    id: 4,
+    label: 'bunch(es)',
+    value: 'bunch(es)',
+  },
+  {
+    id: 5,
+    label: 'cup(s)',
+    value: 'cup(s)',
+  },
+  {
+    id: 6,
+    label: 'carton(s)',
+    value: 'carton(s)',
+  },
+  {
+    id: 7,
+    label: 'crate(s)',
+    value: 'crate(s)',
+  },
+  {
+    id: 8,
+    label: 'finger(s)',
+    value: 'finger(s)',
+  },
+  {
+    id: 9,
+    label: 'kilo(s)',
+    value: 'kilo(s)',
+  },
+  {
+    id: 10,
+    label: 'large',
+    value: 'large',
+  },
+  {
+    id: 11,
+    label: 'litre(s)',
+    value: 'litre(s)',
+  },
+  {
+    id: 12,
+    label: 'medium',
+    value: 'medium',
+  },
+  {
+    id: 13,
+    label: 'packet(s)',
+    value: 'packet(s)',
+  },
+  {
+    id: 14,
+    label: 'piece(s)',
+    value: 'piece(s)',
+  },
+  {
+    id: 15,
+    label: 'roll(s)',
+    value: 'roll(s)',
+  },
+  {
+    id: 16,
+    label: 'sachet(s)',
+    value: 'sachet(s)',
+  },
+  {
+    id: 17,
+    label: 'small',
+    value: 'small',
+  },
+  {
+    id: 18,
+    label: 'tuber(s)',
+    value: 'tuber(s)',
+  },
+];
